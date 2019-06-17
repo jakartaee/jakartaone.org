020b57f1132c922472243ef06a42f5b00db736d4
Christopher Guindon (from Travis CI) <chris.guindon@eclipse-foundation.org>
2019-06-17 19:48:37 +0000

    Deploy jakartaone.org to github.com/jakartaee/jakartaone.org.git:gh-pages

--- js/home.slider.js
+++ js/home.slider.js
@@ -1,3 +1,15 @@
+/** 
+  Copyright (c) 2019 Eclipse Foundation, Inc.
+
+  This program and the accompanying materials are made available under the
+  terms of the Eclipse Public License v. 2.0 which is available at
+  http://www.eclipse.org/legal/epl-2.0.
+
+  Contributors:
+    Martin Lowe <martin.lowe@eclipse-foundation.org>
+
+  SPDX-License-Identifier: EPL-2.0
+*/
 $(document).ready(function () {
     // initialize the Owl carousel
     $("#slider").owlCarousel({
