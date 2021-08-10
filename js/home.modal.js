900109e15a7c0f7d25a145bcd84622761da111cb
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2021-08-10 09:45:10 -0400

    update theme (#321)
    * update theme
    
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>
    
    * update theme
    
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- js/home.modal.js
+++
@@ -1,8 +0,0 @@
-$('#eclipsefdn-modal').on('show.bs.modal', function (event) {
-	  var $init = $(event.relatedTarget);
-	  var $modal = $(this);
-	  $modal.find('h4.modal-title').text($init.attr('data-title'));
-	  $modal.find('#modal-presenter').text($init.attr('data-presenter'));
-	  $modal.find('#modal-time').text($init.attr('data-time'));
-	  $modal.find('.modal-body').html($init.find('.modal-content')[0].innerHTML);
-});
